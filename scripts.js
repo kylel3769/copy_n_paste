a843083a34659d50fe108af0ce68af53fda1bcd7
Kyle Lam <kylel220811@gmail.com>
2024-05-29 17:46:02 -0400

    update

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function copyText() {
+
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("Unfortunately, your infection has progressed too far. You are now designated for termination. Please hold still.");
+}
+
+
+function copyText2() {
+
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("Stay inside and off the grey lines.");
+}
+
+
+function copyText3() {
+
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("You are being terminated for trespassing.");
+}
+
+
+function copyText4() {
+
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("Do not put the wrong inputs.");
+}
+
+
+function copyText5() {
+
+    /* Copy text into clipboard */
+    navigator.clipboard.writeText
+        ("Do not put the wrong inputs.");
+}
